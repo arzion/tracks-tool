ffe41a12f82129d60a1de009203bd36ac9a60142
vladimir.muzyka <vo.muzyka@gmail.com>
2016-07-11 00:41:57 +0300

    Do changes according to API result

--- frontend/bmatTestToolApi.js
+++ frontend/bmatTestToolApi.js
@@ -3,55 +3,41 @@
 import ajax from './utils/ajax';
 
 let actions = {
-    search: null
+    trackSearchTemplate: null
 };
 
 let api = {
     init: (rootUrl) => {
         ajax.get(rootUrl,
         (result) => {
-            actions.search = 'http://search.catalogApi';
-            console.log(`CATALOG API: Initialised. GET from ${rootUrl}. Result: ${result}`);
+            result.links.forEach((link, index) => {
+                if(link.rel === 'catalog-track-search') {
+                    actions.trackSearchTemplate = link.href;
+                }
+            });
+            console.log(`API: Initialised.
+             trackSearchTemplate: ${actions.trackSearchTemplate}`);
         });
     },
 
     search: (data, callback) => {
-        ajax.get(actions.searchTracks, (result) => {
-            console.log(`CATALOG API: Search initiated by: ${actions.search}.
-                Query: ${data.query}. Type: ${data.type}. Portal: ${data.portal} Result: ${result}`);
-            if (typeof callback == 'function') {
-                callback([{
-                    isrc: 100,
-                    lmid: 200,
-                    trackName: 'Hello',
-                    artistName: 'Adele',
-                    albumName: 'The best',
-                    duration: 250,
-                    sample: 'https://cs7-2v4.vk-cdn.net/p24/d31c2fc11feb9d.mp3'
-                }, {
-                    isrc: 101,
-                    lmid: 201,
-                    trackName: 'Hit me baby one more time',
-                    artistName: 'Britney Spears',
-                    albumName: 'Toxic',
-                    duration: 212
-                }, {
-                    isrc: 102,
-                    lmid: 202,
-                    trackName: 'Crip',
-                    artistName: 'Radiohead',
-                    albumName: 'Crip',
-                    duration: 341
-                }, {
-                    isrc: 103,
-                    lmid: 203,
-                    trackName: 'Love is live',
-                    artistName: 'Madonna',
-                    albumName: 'TThis summer',
-                    duration: 283
-                }]);
-            }
-        });
+        if(data.type === 'track') {
+            var url = actions.trackSearchTemplate
+                .replace('{portal}', data.portal)
+                .replace('{query}', data.query);
+            ajax.get(url, (result) => {
+                console.log(`API: Search initiated by ${url}.
+                    Query: ${data.query}. Type: ${data.type}. Portal: ${data.portal}`);
+                if (typeof callback == 'function') {
+                    callback(result);
+                } else {
+                    throw new Error('Callback is not a function.');
+                }
+            });
+        } else {
+            throw new Error(`Type is not supported: ${data.type}`);
+        }
+
     }
 };
 
--- frontend/utils/ajax.js
+++ frontend/utils/ajax.js
@@ -1,9 +1,22 @@
 'use strict';
 
 let ajax = {
-    get: (url, callback) => {
-        if (typeof callback == 'function') {
-            callback('Ajax_Response_GET');
+    get: (url, callback, error) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (this.readyState != 4) {
+                return;
+            }
+            if (this.status != 200) {
+                if (typeof error == 'function') {
+                    error(this);
+                }
+            }
+            if (typeof callback == 'function') {
+                callback(JSON.parse(this.responseText));
+            }
         }
     },
 
